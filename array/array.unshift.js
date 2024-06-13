3d3df02e9cee239a7036770a08b0ed75b21d691c
Satish5815 <satish.kushwaha@zopsmart.com>
2024-06-13 16:49:17 +0530

    feat:js basic

---
+++ array/array.unshift.js
@@ -0,0 +1,4 @@
+//The unshift() add the element at beggning and return the length
+let bike = ["yamaha", "splender", "hunter", "rajdoot"];
+
+console.log(bike.unshift("Pulsar", "Apache"));
---
+++ basic/hoisting.js
@@ -0,0 +1,23 @@
+/**
+ * Example 1
+ */
+getName();
+console.log(x);
+var x = 7;
+function getName() {
+  console.log("Satish kushwaha");
+}
+
+/**
+ * Example 2
+ */
+
+// getName1();
+
+console.log(getName1);
+var getName1 = () => {
+  console.log("Get name 2");
+};
+
+const b = {};
+console.log(b);
